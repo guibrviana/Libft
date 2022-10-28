ba93b7ada2ce8d5a0ab624c4c8a8244c786f87b2
Guilherme Viana <guilhermebrviana@gmail.com>
2022-10-28 11:55:05 +0100

    .

---
+++ ft_isalpha.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gubranco <gubranco@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/28 11:35:44 by gubranco          #+#    #+#             */
+/*   Updated: 2022/10/28 11:51:00 by gubranco         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	return (0);
+}
+
+/* checar se o int passado como parametro é um caractere alfabetico,
+caso seja verdadeiro retorna 1, caso negativo, retorna 0 */
