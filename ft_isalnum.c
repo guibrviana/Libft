ac9639862637b792be3058d69afe1ecdaabf1744
Guilherme Viana <guilhermebrviana@gmail.com>
2022-10-28 12:08:36 +0100

    .

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,23 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gubranco <gubranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 11:35:44 by gubranco          #+#    #+#             */
-/*   Updated: 2022/10/28 11:56:22 by gubranco         ###   ########.fr       */
+/*   Created: 2022/10/28 11:59:44 by gubranco          #+#    #+#             */
+/*   Updated: 2022/10/28 12:08:13 by gubranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+int	ft_isalnum(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')
+		|| (c >= '0' && c <= '9'))
 		return (1);
 	return (0);
 }
 
-/* checks if the parameter int is an alphabetic character, if its true it
-returns 1, if its false it returns 0 */
+/* checks if the parameter int is a decimal digit or alphabetical character, 
+if its true it returns 1, if its false it returns 0 */
--- ft_isdigit.c
+++ ft_isdigit.c
@@ -1,23 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isalpha.c                                       :+:      :+:    :+:   */
+/*   ft_isdigit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: gubranco <gubranco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/28 11:35:44 by gubranco          #+#    #+#             */
-/*   Updated: 2022/10/28 11:56:22 by gubranco         ###   ########.fr       */
+/*   Created: 2022/10/28 11:56:43 by gubranco          #+#    #+#             */
+/*   Updated: 2022/10/28 11:58:24 by gubranco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha(int c)
+int	ft_isdigit(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
 
-/* checks if the parameter int is an alphabetic character, if its true it
+/* checks if the parameter int is a decimal digit character, if its true it
 returns 1, if its false it returns 0 */
